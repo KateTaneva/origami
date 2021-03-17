3a0ed7365b4f869bc190b7491a23429d9f8e6696
Katerina Taneva <katerina.tanewa@gmail.com>
2021-03-17 15:54:55 +0200

    Main, Header, Asside add

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import Header from "./components/Header";
+import style from "./App.module.css";
+import Menu from './components/Menu';
+import Main from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className={style.app}>
+//       <Header />
+//       <div className={style.container}>
+//       <Menu />
+//      <Main />
+//     </div>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render(){
+    return(
+      <div className={style.app}>
+      <Header />
+      <div className={style.container}>
+      <Menu />
+     <Main />
     </div>
-  );
+    </div>
+    );
+  }
 }
 
 export default App;
